3b546a82edb5561279c54c8531ce12363c4a68d0
mitchspiron <mitchspiron280@gmail.com>
2022-08-18 22:43:30 +0300

    update: procfile

--- db/migrations/20220813195904_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddForeignKey
-ALTER TABLE `commande` ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`numClient`) REFERENCES `client`(`numClient`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `commande` ADD CONSTRAINT `commande_ibfk_2` FOREIGN KEY (`numProduit`) REFERENCES `produit`(`numProduit`) ON DELETE CASCADE ON UPDATE CASCADE;
